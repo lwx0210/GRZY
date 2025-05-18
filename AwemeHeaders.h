a6a39c3ec1d9246eb4c95578f802ef6b9cf2789a
Lee Moon hee <ways0210@qq.com>
2025-05-18 15:45:03 +0800

    Update AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -1058,5 +1058,7 @@ typedef NS_ENUM(NSInteger, MediaType) {
 @property (nonatomic, strong) UITabBarController *tabBarController;
 @end
 
-
+@interface AWEIMCommentShareUserHorizontalSectionController : UIViewController
+- (void)configCell:(id)cell index:(NSInteger)index model:(id)model;
+@end
 
