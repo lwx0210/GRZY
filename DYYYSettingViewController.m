06ea8c5f2d667c55afbc0175b19bc2b5e4bae9e8
Lee Moon hee <ways0210@qq.com>
2025-05-29 00:17:12 +0800

    Update DYYYSettingViewController.m

--- DYYYSettingViewController.m
+++ DYYYSettingViewController.m
@@ -181,7 +181,7 @@ typedef NS_ENUM(NSInteger, DYYYSettingItemType) {
             [DYYYSettingItem itemWithTitle:@"设置朋友标题" key:@"DYYYFriendsTitle" type:DYYYSettingItemTypeTextField placeholder:@"不填默认"],
             [DYYYSettingItem itemWithTitle:@"设置消息标题" key:@"DYYYMsgTitle" type:DYYYSettingItemTypeTextField placeholder:@"不填默认"],
             [DYYYSettingItem itemWithTitle:@"设置我的标题" key:@"DYYYSelfTitle" type:DYYYSettingItemTypeTextField placeholder:@"不填默认"],
-            [DYYYSettingItem itemWithTitle:@"自定义总开关" key:@"DYYYEnableSocialStatsCustom" type:DYYYSettingItemTypeSwitch],
+            [DYYYSettingItem itemWithTitle:@"个人自定开关" key:@"DYYYEnableSocialStatsCustom" type:DYYYSettingItemTypeSwitch],
             [DYYYSettingItem itemWithTitle:@"获赞数量自定" key:@"DYYYCustomLikes" type:DYYYSettingItemTypeTextField placeholder:@"不填默认"],
             [DYYYSettingItem itemWithTitle:@"互关数量自定" key:@"DYYYCustomMutual" type:DYYYSettingItemTypeTextField placeholder:@"不填默认"],
             [DYYYSettingItem itemWithTitle:@"关注数量自定" key:@"DYYYCustomFollowing" type:DYYYSettingItemTypeTextField placeholder:@"不填默认"],
