3dfc65bc5da6e8e0c7d5c35cb92ce98648542cb7
Lee Moon hee <ways0210@qq.com>
2025-06-03 19:24:56 +0800

    Update AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -1074,23 +1074,6 @@ static CGFloat gStartVal = 0.0;
 + (id)sharedManager;
 @end
 
-// 视频播放控制处理器
-@interface AWEPlayerPlayControlHandler : NSObject
-@property(nonatomic, strong) AVAudioUnitEQ *audioEQ;
-@property(nonatomic, strong) AVAudioUnitReverb *reverb;
-@property(nonatomic, assign) BOOL noiseFilterEnabled;
-- (void)setupNoiseFilter;
-- (void)addNoiseFilterButton;
-- (void)toggleNoiseFilter;
-- (void)setupAVPlayerItem:(AVPlayerItem *)item;
-- (id)player;
-@end
-
-// 视频控制视图
-@interface AWEFeedVideoControlView : UIView
-- (void)handleVideoQualityLongPress:(UILongPressGestureRecognizer *)gesture;
-@end
-
 //直播默认最高清晰度
 @interface IESLiveRoomComponent : NSObject
 @end
