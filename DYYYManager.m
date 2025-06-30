37e9040a0664ee2c28a3ec06b2b4d0189cbc837b
Lee Moon hee <ways0210@qq.com>
2025-07-01 02:16:41 +0800

    Add files via upload

--- DYYYManager.m
+++ DYYYManager.m
@@ -10,9 +10,48 @@
 #import <libwebp/mux.h>
 #import <objc/message.h>
 #import <objc/runtime.h>
+#ifndef kCGImagePropertyHEIFDictionary
+#define kCGImagePropertyHEIFDictionary CFSTR("HEIFDictionary")
+#endif
+#ifndef kCGImagePropertyHEIFDelayTime
+#define kCGImagePropertyHEIFDelayTime CFSTR("DelayTime")
+#endif
+#ifndef kUTTypeHEIC
+#define kUTTypeHEIC CFSTR("public.heic")
+#endif
 
 #import "DYYYToast.h"
 #import "DYYYUtils.h"
+
+static const NSTimeInterval kDYYYDefaultFrameDelay = 0.1f;
+static const CGFloat kDYYYMillisecondsPerSecond = 1000.0f;
+
+static inline CGFloat DYYYFrameDelayForProperties(CFDictionaryRef properties) {
+  CGFloat delayTime = kDYYYDefaultFrameDelay;
+  if (properties) {
+    CFDictionaryRef gifDict =
+        CFDictionaryGetValue(properties, kCGImagePropertyGIFDictionary);
+    if (gifDict) {
+      CFNumberRef delayNum =
+          CFDictionaryGetValue(gifDict, kCGImagePropertyGIFDelayTime);
+      if (delayNum)
+        CFNumberGetValue(delayNum, kCFNumberFloatType, &delayTime);
+    } else {
+      CFDictionaryRef heifDict =
+          CFDictionaryGetValue(properties, kCGImagePropertyHEIFDictionary);
+      if (heifDict) {
+        CFNumberRef delayNum =
+            CFDictionaryGetValue(heifDict, kCGImagePropertyHEIFDelayTime);
+        if (delayNum)
+          CFNumberGetValue(delayNum, kCFNumberFloatType, &delayTime);
+      }
+    }
+  }
+  if (delayTime <= 0.0f) {
+    delayTime = kDYYYDefaultFrameDelay;
+  }
+  return delayTime;
+}
 
 @interface DYYYManager () {
   AVAssetExportSession *session;
@@ -416,7 +455,8 @@ static void ReleaseWebPData(void *info, const void *data, size_t size) {
 
           NSDictionary *frameProperties = @{
             (__bridge NSString *)kCGImagePropertyGIFDictionary : @{
-              (__bridge NSString *)kCGImagePropertyGIFDelayTime : @0.1f,
+              (__bridge NSString *)kCGImagePropertyGIFDelayTime :
+                  @(kDYYYDefaultFrameDelay),
             }
           };
 
@@ -589,7 +629,10 @@ static void ReleaseWebPData(void *info, const void *data, size_t size) {
             CGImageRef canvasImageRef = CGBitmapContextCreateImage(currCanvas);
 
             // 处理帧间延迟
-            float delayTime = iter.duration / 1000.0f;
+            CGFloat delayTime =
+                iter.duration > 0
+                    ? iter.duration / kDYYYMillisecondsPerSecond
+                    : kDYYYDefaultFrameDelay;
 
             // 创建帧属性
             NSDictionary *frameProperties = @{
@@ -672,8 +715,14 @@ static void CGContextCopyBytes(CGContextRef dst, CGContextRef src, int width,
   dispatch_async(
       dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         // 1. 创建ImageSource
+        NSDictionary *options = @{
+            (__bridge NSString *)kCGImageSourceTypeIdentifierHint :
+                (__bridge NSString *)kUTTypeHEIC,
+            (__bridge NSString *)kCGImageSourceShouldCache : @NO
+        };
         CGImageSourceRef src =
-            CGImageSourceCreateWithURL((__bridge CFURLRef)heicURL, NULL);
+            CGImageSourceCreateWithURL((__bridge CFURLRef)heicURL,
+                                      (__bridge CFDictionaryRef)options);
         if (!src) {
           dispatch_async(dispatch_get_main_queue(), ^{
             if (completion)
@@ -684,7 +733,6 @@ static void CGContextCopyBytes(CGContextRef dst, CGContextRef src, int width,
 
         // 2. 获取帧数
         size_t count = CGImageSourceGetCount(src);
-        BOOL isAnimated = (count > 1);
 
         // 3. 生成GIF路径
         NSString *gifFileName =
@@ -719,20 +767,10 @@ static void CGContextCopyBytes(CGContextRef dst, CGContextRef src, int width,
           CGImageRef imgRef = CGImageSourceCreateImageAtIndex(src, i, NULL);
 
           // 获取帧延迟
-          float delayTime = 0.1f;
           CFDictionaryRef properties =
               CGImageSourceCopyPropertiesAtIndex(src, i, NULL);
-          if (properties) {
-            CFDictionaryRef gifDict =
-                CFDictionaryGetValue(properties, kCGImagePropertyGIFDictionary);
-            if (gifDict) {
-              CFNumberRef delayNum =
-                  CFDictionaryGetValue(gifDict, kCGImagePropertyGIFDelayTime);
-              if (delayNum)
-                CFNumberGetValue(delayNum, kCFNumberFloatType, &delayTime);
-            }
-            CFRelease(properties);
-          }
+          CGFloat delayTime = DYYYFrameDelayForProperties(properties);
+          if (properties) CFRelease(properties);
 
           NSDictionary *frameProps = @{
             (__bridge NSString *)kCGImagePropertyGIFDictionary : @{
