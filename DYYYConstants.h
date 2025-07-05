fd146276cd9aec501d7f0eea449de4e0c3a7bae4
Lee Moon hee <ways0210@qq.com>
2025-07-05 17:16:03 +0800

    Add files via upload

--- DYYYConstants.h
+++ DYYYConstants.h
@@ -1,12 +1,21 @@
 #ifndef DYYYConstants_h
 #define DYYYConstants_h
 
-// 插件名称常量
 #define DYYY_NAME @"DYYY"
 #define DYYY_SETTINGS_NAME @"DYYY设置"
 
-// 版本信息
-#define DYYY_VERSION @"2.3.0"
-#define DYYY_VERSION_STRING @"v2.3.0"
+#define DYYY_VERSION @"2.2-8"
+
+// 默认的远程 ABTest 配置地址
+#define DYYY_DEFAULT_ABTEST_URL @"https://github.com/Nathalie-Annis/AWEABTestDataPatch/releases/latest/download/ABTestDataPatch_A.json"
+
+// 是否使用远程配置的偏好键
+#define DYYY_REMOTE_CONFIG_FLAG_KEY @"DYYYUseRemoteConfig"
+
+// 远程配置状态改变的通知名
+#define DYYY_REMOTE_CONFIG_CHANGED_NOTIFICATION @"DYYYRemoteConfigStateChanged"
+
+// 配置应用方式中的远程模式名称
+#define DYYY_REMOTE_MODE_STRING @"远程模式：启动时自动检查更新"
 
 #endif
